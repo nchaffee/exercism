75c75d73a91d8ff29ec5687fb7b0daf86cbaafdf
Nick Chaffee <nchaffee@gmail.com>
2017-09-27 17:48:13 -0700

    ruby raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,20 @@
+class Integer
+  def factor_of?(product)
+    product % self == 0
+  end
+end
+
+class Raindrops
+  def self.convert number_of_drops
+    raindrop_speak = ""
+    raindrop_speak << "Pling" if 3.factor_of?(number_of_drops)
+    raindrop_speak << "Plang" if 5.factor_of?(number_of_drops)
+    raindrop_speak << "Plong" if 7.factor_of?(number_of_drops)
+    raindrop_speak << number_of_drops.to_s if raindrop_speak.empty?
+    raindrop_speak
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
