2529b810b472f10347668d70deef402d1800570c
Nick Chaffee <nchaffee@gmail.com>
2017-09-27 17:48:46 -0700

    ruby difference-of-squares

---
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,34 @@
+# ruby version 2.3.1 ain't got sum method
+
+class Squares
+  def initialize highest_number
+    @numbers = (1..highest_number)
+    @squares = @numbers.map {|each_number| square_of(each_number)}
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+  def square_of_sum
+    square_of(sum_of(@numbers))
+  end
+
+  def sum_of_squares
+    sum_of(@squares)
+  end
+
+  private
+
+  def sum_of(an_array)
+    an_array.inject(&:+)
+  end
+
+  def square_of(number)
+    number**2
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
