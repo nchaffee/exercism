26b6903af577e9cf963b7cc780adebab6331b24c
Nick Chaffee <nchaffee@gmail.com>
2017-09-27 17:51:47 -0700

    ruby bob

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,26 @@
+class Bob
+  ANSWERS = {
+      yell:     'Whoa, chill out!',
+      question: 'Sure.',
+      nothing:  'Fine. Be that way!'
+  }.tap {|answers| answers.default = 'Whatever.'}
+
+  def self.hey remark
+    remark.strip!
+
+    remark_type =
+        if remark.match(/[[:upper:]]/) && remark == remark.upcase
+          :yell
+        elsif remark.end_with?("?")
+          :question
+        elsif remark.empty?
+          :nothing
+        end
+    ANSWERS[remark_type]
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
+
