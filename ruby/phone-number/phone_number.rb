6b6d8be8d59f8f7d9739516a0189983f20298fd3
Nick Chaffee <nchaffee@gmail.com>
2017-10-13 21:11:56 -0700

    ruby phone-number 3

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,24 +1,24 @@
 class PhoneNumber
-  ALLOWED_EXCHANGE_DIGITS = ('2'..'9').freeze
   ALLOWED_DIGITS = ('0'..'10').freeze
+  ALLOWED_EXCHANGE_DIGITS = ('2'..'9').freeze
 
   def self.clean(submitted_number)
-    phone_number = submitted_number
-                     .chars
-                     .select { |digit| ALLOWED_DIGITS.include?(digit) }
-                     .join
-    phone_number[0] = '' if phone_number[0] == '1'
-
-    Validator.validate(phone_number)
+    new(submitted_number).validate
   end
 
-  class Validator
-    def self.validate(phone_number)
-      valid_size = phone_number.size == 10
-      valid_exchange_code = ALLOWED_EXCHANGE_DIGITS.include?(phone_number[3])
+  def initialize(phone_number)
+    @phone_number = phone_number
+                    .chars
+                    .select { |digit| ALLOWED_DIGITS.include?(digit) }
+                    .join
+                    .tap { |number| number[0] = '' if number[0] == '1' }
+  end
 
-      valid_size && valid_exchange_code ? phone_number : nil
-    end
+  def validate
+    valid_size = @phone_number.size == 10
+    valid_exchange_code = ALLOWED_EXCHANGE_DIGITS.include?(@phone_number[3])
+
+    valid_size && valid_exchange_code ? @phone_number : nil
   end
 end
 
