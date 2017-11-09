6007996b52e2836f501427a26430c0650968f736
Nick Chaffee <nchaffee@gmail.com>
2017-11-08 18:32:57 -0800

    ruby trinary

---
+++ ruby/trinary/trinary.rb
@@ -0,0 +1,24 @@
+class Trinary
+  VALID_SYMBOLS = [0, 1, 2].freeze
+
+  def initialize symbols
+    @symbols = symbols.chars.all? { |symbol| VALID_SYMBOLS.include?(symbol.to_i) } ? symbols.chars.map(&:to_i) : []
+    @values = []
+    next_value_from(@symbols.dup)
+  end
+
+  def to_decimal
+    @values.sum
+  end
+
+  private
+
+  def next_value_from(symbols)
+    @values << symbols.pop * (3**(@values.length))
+    next_value_from(symbols) if symbols.any?
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
