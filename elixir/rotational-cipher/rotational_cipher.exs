a69281b31806042d826a586c24606805e8f839b9
Nick Chaffee <nchaffee@gmail.com>
2018-03-21 15:13:36 -0700

    elixir rotational-cipher 3

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -11,13 +11,11 @@ defmodule RotationalCipher do
     text
     |> String.to_charlist
     |> Enum.map(&rotate_char(shift + &1, &1))
-    |> Enum.map(&stringify/1)
+    |> Enum.map(&to_string/1)
     |> Enum.join
   end
 
-  defp rotate_char(shifted, old) when old in 96..123, do: rem((shifted - 97),26) + 97
-  defp rotate_char(shifted, old) when old in 64..91, do: rem((shifted - 65),26) + 65
-  defp rotate_char(_, char), do: char
-
-  defp stringify(char), do: to_string([char])
+  defp rotate_char(shifted, old) when old in 96..123, do: [rem((shifted - 97),26) + 97]
+  defp rotate_char(shifted, old) when old in 64..91, do: [rem((shifted - 65),26) + 65]
+  defp rotate_char(_, char), do: [char]
 end
