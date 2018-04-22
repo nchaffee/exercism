dceb68f41b547ba36ec9414d3e5820aa4bcc430b
Nick Chaffee <nchaffee@gmail.com>
2018-04-21 17:54:59 -0700

    elm hello-world 2

--- elm/hello-world/HelloWorld.elm
+++ elm/hello-world/HelloWorld.elm
@@ -1,15 +1,5 @@
 module HelloWorld exposing (helloWorld)
 
-mightBeANameHere : Maybe String -> String
-mightBeANameHere str =
-    case str of
-    Nothing ->
-      "World"
-
-    Just val ->
-      val
-
-
 helloWorld : Maybe String -> String
 helloWorld name =
-    String.concat ["Hello, ", (mightBeANameHere name), "!"]
+    "Hello, " ++ Maybe.withDefault "World" name ++ "!"
