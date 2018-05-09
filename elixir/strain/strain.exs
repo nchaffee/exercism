db882a1239cf9d7e43b79982215fca312781a752
Nick Chaffee <nchaffee@gmail.com>
2018-05-09 09:01:13 -0700

    elixir strain 2

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -1,23 +1,11 @@
 defmodule Strain do
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns true.
-
-  Do not use `Enum.filter`.
-  """
-  @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def keep(list, function) do
-    for item <- list, function.(item), do: item
+  def keep([], _function), do: []
+  def keep([head | tail], function) do
+    if function.(head), do: [head | keep(tail, function)], else: keep(tail, function)
   end
 
-  @doc """
-  Given a `list` of items and a function `fun`, return the list of items where
-  `fun` returns false.
-
-  Do not use `Enum.reject`.
-  """
-  @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
-  def discard(list, function) do
-    for item <- list, !function.(item), do: item
+  def discard([], _function), do: []
+  def discard([head | tail], function) do
+    if !function.(head), do: [head | discard(tail, function)], else: discard(tail, function)
   end
 end
