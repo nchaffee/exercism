132c5f236e2399786df8fa55882c887df6c922f6
Nick Chaffee <nchaffee@gmail.com>
2018-05-10 08:42:19 -0700

    elixir strain 4

--- elixir/strain/strain.exs
+++ elixir/strain/strain.exs
@@ -1,10 +1,10 @@
 defmodule Strain do
   def keep([], _function), do: []
   def keep([head | tail], function) do
-    if function.(head), do: [head | keep(tail, function)], else: keep(tail, function)
+    if apply(function, [head]), do: [head | keep(tail, function)], else: keep(tail, function)
   end
 
   def discard(list, function) do
-    list -- keep(list, function)
+    keep(list, &(!apply(function, [&1])))
   end
 end
