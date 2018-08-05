07cc837f7d963f67e8b1e5e236f1dd1d9b6d55ab
Nick Chaffee <nchaffee@gmail.com>
2018-08-04 18:12:41 -0700

    elixir bob 2

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,51 +1,34 @@
 defmodule Bob do
-  @space 32
-  @question_mark 63
-  
-  def hey(input) do
+  def hey(str) do
   	cond do
-  	  is_empty(input) ->
+  	  empty?(str) ->
   	  	"Fine. Be that way!"
-  	  is_question(input) && is_all_caps(input) ->
+  	  question?(str) && all_caps?(str) ->
   	  	"Calm down, I know what I'm doing!"
-  	  is_question(input) ->
+  	  question?(str) ->
   	  	"Sure."
-  	  is_all_caps(input) ->
+  	  all_caps?(str) ->
   	  	"Whoa, chill out!"
 	  true ->
 	  	"Whatever."
   	end
   end
 
-  defp is_empty(str) do
-  	str
-  	|> String.to_charlist()
-  	|> Enum.reject(&(&1 == @space))
-  	|> Enum.empty?()
+  defp empty?(str), do: String.trim(str) == ""
+  defp question?(str), do: String.last(str) == "?"
+
+  defp all_caps?(str) do
+  	letters = str |> String.to_charlist() |> Enum.filter(&(letter?(&1)))
+  	Enum.all?(letters, &(upcase?(<<&1>>))) && length(letters) > 0
   end
 
-  defp is_question(str) do
-  	last_char = 
-	  	str
-	  	|> String.to_charlist()
-		|> Enum.reverse()
-		|> List.first()
-	last_char == @question_mark
+  defp letter?(char) do
+  	!(MapSet.new(32..64)
+  	  	|> MapSet.union(MapSet.new(91..96))
+  	  	|> MapSet.union(MapSet.new(123..126)) 
+  	  	|> MapSet.to_list
+  	  	|> Enum.member?(char))
   end
 
-  defp is_all_caps(str) do
-  	letters = 
-	  	str
-	  	|> String.to_charlist()
-	  	|> Enum.filter(&(is_letter(&1)))
-  	Enum.all?(letters, &(is_upcase(<<&1>>))) && length(letters) > 0
-  end
-
-  defp is_letter(char) do
-  	!Enum.member?(32..64, char) && !Enum.member?(91..96, char) && !Enum.member?(123..126, char)
-  end
-
-  defp is_upcase(letter) do
-  	letter == String.upcase(letter)
-  end
+  defp upcase?(letter), do: letter == String.upcase(letter)
 end
