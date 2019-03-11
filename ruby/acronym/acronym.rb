fe93e719e115ae321c5d6470214b97ef21144507
Nick C <ndc@Nicks-MacBook-Pro.local>
2019-03-11 23:10:13 +0700

    ruby acronym 2

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,9 +1,14 @@
+class String
+  def first; self[0]; end
+end
+
 class Acronym
   def self.abbreviate(phrase)
     phrase.
     gsub('-', ' ').
     split(' ').
-    map{|snippet| snippet[0].upcase}.
+    map(&:first).
+    map(&:upcase).
     join
   end
 end
