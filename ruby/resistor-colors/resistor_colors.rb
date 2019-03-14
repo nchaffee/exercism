cbe760a57db97d9112013462ba6bc7169fa4d0a4
Nick Chaffee <nchafee@gmail.com>
2019-03-14 22:44:49 +0700

    ruby resistor-colors 2

--- ruby/resistor-colors/resistor_colors.rb
+++ ruby/resistor-colors/resistor_colors.rb
@@ -1,19 +1,19 @@
 class ResistorColors
   COLORS = {
-    "black" => "0",
-    "brown" => "1",
-    "red" => "2",
-    "orange" => "3",
-    "yellow" => "4",
-    "green" => "5",
-    "blue" => "6",
-    "violet" => "7",
-    "grey" => "8",
-    "white" => "9"
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
   }
   def self.value colors
-    colors.inject("") do |acc, color| 
-      acc + COLORS[color]
-    end.to_i
+    result = ""
+    colors.each {|color| result << COLORS[color].to_s}
+    result.to_i
   end
 end
