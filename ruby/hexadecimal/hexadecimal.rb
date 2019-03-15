e87fb3199616e2fc2a602f8abe741a7a3260a172
Nick Chaffee <nchafee@gmail.com>
2019-03-15 16:41:57 +0700

    ruby hexadecimal 3

--- ruby/hexadecimal/hexadecimal.rb
+++ ruby/hexadecimal/hexadecimal.rb
@@ -1,21 +1,17 @@
 class Hexadecimal
   HEX_TO_DECIMAL = {"0" => 0, "1" => 1, "2" => 2, "3" => 3, "4" => 4, "5" => 5, "6" => 6, "7" => 7, "8" => 8, "9" => 9, "a" => 10, "b" => 11, "c" => 12, "d" => 13, "e" => 14, "f" => 15}
-
+  
+  attr_reader :to_decimal
+  
   def initialize hex_string
-    @decimal_values = hex_string.chars.reverse.map do |char|
-      HEX_TO_DECIMAL[char]
-    end
-  end
-
-  def to_decimal
-    @decimal_values.any?(&:nil?) ? 0 : sum_decimal_values
-  end
-
-  private
-
-  def sum_decimal_values
-    @decimal_values.map.with_index do |decimal_value, index|
-      decimal_value * 16.pow(index)
-    end.sum
+    @to_decimal = 
+      hex_string.
+      chars.
+      reverse.
+      map {|char| HEX_TO_DECIMAL[char]}.
+      map.with_index do |decimal_value, index|
+        decimal_value.nil? ? (break [0]) : decimal_value * 16.pow(index)
+      end.
+      sum
   end
 end
