0333fa8b2f770cab094c2113df127a1842eaf98c
Nick Chaffee <nchafee@gmail.com>
2019-03-19 08:54:59 +0700

    ruby matrix 2

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,20 +1,15 @@
 class Matrix
-  def initialize matrix_text
+  def initialize(matrix_text)
     @matrix_text = matrix_text
   end
 
   def rows
-    @matrix_text.
-    split("\n").
-    map{|row| row.split(" ").map(&:to_i)}
+    @matrix_text.lines.map do |line|
+      line.split.map(&:to_i)
+    end
   end
 
   def columns
-    rows.inject(Array.new(rows.size) { Array.new }) do |result, row|
-      row.each_with_index do |element, index|
-        result[index] << element
-      end
-      result
-    end
+    rows.transpose
   end
 end
