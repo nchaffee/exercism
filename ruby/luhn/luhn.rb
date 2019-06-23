802a5d4598080be411105f8ed0ce2405c7d5f674
Nick Chaffee <nchafee@gmail.com>
2019-06-22 17:02:03 -0700

    ruby luhn 2

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,8 +1,33 @@
 class Luhn
-  def self.valid?(number_str)
-    number_str = number_str.gsub(" ", "")
-    number_str.length > 1 &&
-      number_str.match?(/^\d*$/) &&
-      ((number_str.chars.map(&:to_i).reverse.map.with_index{|num, idx| (((idx+1) % 2) == 0) ? (num *2 >= 10 ? (num*2 % 10) + 1 : num*2) : num}.sum % 10) == 0)
+  class << self
+    def valid?(str)
+      str = str.gsub(" ", "")
+      longer_than_one?(str) && only_numbers?(str) && valid_checksum?(str)
+    end
+
+    def valid_checksum?(str)
+      digits = str.chars.map(&:to_i)
+      convert_to_doubles_and_sum(digits) % 10 == 0
+    end
+
+    def convert_to_doubles_and_sum(str)
+      str.reverse.map.with_index do |number, index|
+        second_digit?(index) ? limit_to_one_digit(number*2) : number
+      end.sum
+    end
+
+    def limit_to_one_digit(number)
+      more_than_10?(number) ? add_digits(number) : number
+    end
+
+    def more_than_10?(number) number >= 10; end
+
+    def second_digit?(index) (index + 1) % 2 == 0; end
+
+    def add_digits(number) number % 10 + 1; end
+
+    def longer_than_one?(str) str.length > 1; end
+
+    def only_numbers?(str) str.match?(/^\d*$/); end
   end
 end
