5f025fae2ce38ce55d11f4e1bc038fb103484339
Nick Chaffee <nchafee@gmail.com>
2019-06-24 10:51:41 -0700

    js resistor-color 2

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,6 +1,5 @@
 export const colorCode = (color_str) => {
-  const number = (color_str === "white") ? 9 : ((color_str === "orange") ? 3 : 0);
-  return number;
+  return COLORS.indexOf(color_str);
 };
 
 export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
