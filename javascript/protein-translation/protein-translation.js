23c3800d40613b9eff3f7cb989e2205ef5e9dae0
Nick Chaffee <nchafee@gmail.com>
2019-06-26 11:56:58 -0700

    js protein-translation

---
+++ javascript/protein-translation/protein-translation.js
@@ -0,0 +1,41 @@
+const PROTEINS = {
+  'AUG':'Methionine',
+  'UUU':'Phenylalanine',
+  'UUC':'Phenylalanine',
+  'UUA':'Leucine',
+  'UUG':'Leucine',
+  'UCU':'Serine',
+  'UCC':'Serine',
+  'UCA':'Serine',
+  'UCG':'Serine',
+  'UAU':'Tyrosine',
+  'UAC':'Tyrosine',
+  'UGU':'Cysteine',
+  'UGC':'Cysteine',
+  'UGG':'Tryptophan',
+  'UAA':'STOP',
+  'UAG':'STOP',
+  'UGA':'STOP'
+}
+
+const ribosome = (codons, polypeptide = []) => {
+  if (codons.length == 0) {
+    return polypeptide;
+  } else {
+    let protein = PROTEINS[codons.shift()];
+    switch (protein) {
+      case undefined:
+        throw new Error('Invalid codon');
+      case 'STOP':
+        return polypeptide;
+      default:
+        polypeptide.push(protein);
+        return ribosome(codons, polypeptide);
+    }
+  }
+}
+
+export const translate = (str) => {
+  let codons = str ? str.match(/.{3}/g) : [];
+  return ribosome(codons);
+}
