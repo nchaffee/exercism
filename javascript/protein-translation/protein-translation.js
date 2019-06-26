6fa1bce5afc9d55986ac8864d1f7491d6207e9ea
Nick Chaffee <nchafee@gmail.com>
2019-06-26 12:16:45 -0700

    js protein-translation 2

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -19,23 +19,21 @@ const PROTEINS = {
 }
 
 const ribosome = (codons, polypeptide = []) => {
-  if (codons.length == 0) {
-    return polypeptide;
-  } else {
-    let protein = PROTEINS[codons.shift()];
-    switch (protein) {
-      case undefined:
-        throw new Error('Invalid codon');
-      case 'STOP':
-        return polypeptide;
-      default:
-        polypeptide.push(protein);
-        return ribosome(codons, polypeptide);
-    }
+  if (codons.length == 0) { return polypeptide; }
+
+  let protein = PROTEINS[codons.shift()];
+  switch (protein) {
+    case undefined:
+      throw new Error('Invalid codon');
+    case 'STOP':
+      return polypeptide;
+    default:
+      polypeptide.push(protein);
+      return ribosome(codons, polypeptide);
   }
 }
 
-export const translate = (str) => {
-  let codons = str ? str.match(/.{3}/g) : [];
+export const translate = (rna) => {
+  let codons = rna ? rna.match(/.{3}/g) : [];
   return ribosome(codons);
 }
