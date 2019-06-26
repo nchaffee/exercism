83db80302412498efa8683d9fdf2c03ca578d123
Nick Chaffee <nchafee@gmail.com>
2019-06-25 20:16:13 -0700

    js triangle

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,25 @@
+export class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  kind() {
+    if (this.side1 <= 0 || this.side2 <= 0 || this.side3 <= 0) {
+      throw new Error('illegal');
+    } else if (
+                (this.side1 + this.side2) < this.side3 || 
+                (this.side1 + this.side3) < this.side2 ||
+                (this.side2 + this.side3) < this.side1
+              ) {
+      throw new Error('illegal');
+    } else if (this.side1 == this.side2 && this.side1 == this.side3) {
+      return 'equilateral';
+    } else if (this.side1 == this.side2 || this.side1 == this.side3 || this.side2 == this.side3) {
+      return 'isosceles';
+    } else {
+      return 'scalene';
+    }
+  }
+}
