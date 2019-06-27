6f1d9b4f9c4ac93393ca6be15454ca6c3368e41b
Nick Chaffee <nchafee@gmail.com>
2019-06-26 17:32:12 -0700

    js armstrong-numbers 2

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,9 +1,13 @@
+const toS = num => num.toString()
+const toA = str => Array.from(str)
+const size = num => toS(num).length
+const toI = str => parseInt(str)
+
 const armstrongNumber = (number) => {
-  const num_str = number.toString()
-  return num_str.
-         split('').
-         map(n => Math.pow(parseInt(n), num_str.length)).
-         reduce((acc, num) => acc + num)
+  return toA(toS(number)).
+         map(s => toI(s)).
+         map(n => Math.pow(n, size(number))).
+         reduce((acc, n) => acc + n)
 }
 
 export const validate = (number) => {
