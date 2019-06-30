3febef94d666be92296655d222635d2a65bba54b
Nick Chaffee <nchafee@gmail.com>
2019-06-30 10:41:12 -0400

    js all-your-base 2

--- javascript/all-your-base/all-your-base.js
+++ javascript/all-your-base/all-your-base.js
@@ -6,7 +6,7 @@ export const convert = (bits, inputBase, outputBase) => {
   validateInput(bits, inputBase);
 
   let total = toBaseTen(bits, inputBase);
-  return fromBaseTen(total, outputBase);
+  return fromBaseTen([], outputBase, total);
 };
 
 const validateBase = (inputOrOutputBase, errorMsg) => {
@@ -21,8 +21,7 @@ const validateInput = (bits, inputBase) => {
   if (
     bits.length == 0 ||
     (bits.length > 1 && bits[0] == 0) ||
-    bits.some(b => b < 0) ||
-    bits.some(b => b >= inputBase)
+    bits.some(b => b < 0 || b >= inputBase)
   ) { throw new Error('Input has wrong format') }
 }
 
@@ -30,39 +29,18 @@ const toBaseTen = (bits, base, total = 0) => {
   if (bits.length == 0) {
     return total;
   } else {
-    let bit = bits.shift();
-    let new_total = total + (bit * Math.pow(base, bits.length));
-    return toBaseTen(bits, base, new_total);
+    let totalAccrued = total + (bits.shift() * Math.pow(base, bits.length));
+    return toBaseTen(bits, base, totalAccrued);
   }
 }
 
-const fromBaseTen = (totalRemaining, outputBase) => {
-  let exponent = highestDivisibleExponent(totalRemaining, outputBase);
-
-  let bits = [];
-  while (exponent > 0) {
-    let digitValue = Math.pow(outputBase, exponent);
-    let bitValue = Math.floor(totalRemaining / digitValue);
-
-    bits = appendBit(bits, bitValue);
-    totalRemaining = totalRemaining % digitValue;
-    exponent--;
+const fromBaseTen = (bits = [], outputBase, totalRemaining) => {
+  if (totalRemaining == 0) {
+    if (bits.length == 0) { bits.unshift(0) }
+    return bits;
+  } else {
+    bits.unshift(totalRemaining % outputBase);
+    totalRemaining = Math.floor(totalRemaining / outputBase);
+    return fromBaseTen(bits, outputBase, totalRemaining)
   }
-  bits.push(totalRemaining);
-  return bits;
-}
-
-const highestDivisibleExponent = (total, outputBase) => {
-  let exponent = 0;
-  while (Math.floor(total / Math.pow(outputBase, exponent)) > 1) { exponent++ }
-  return exponent;
-}
-
-const appendBit = (bits, bitValue) => {
-  if (bitValue > 0) {
-    bits.push(bitValue)
-  } else if (bits.length > 0) {
-    bits.push(0)
-  }
-  return bits;
 }
