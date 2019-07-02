ef441ca0baae86bf4df2c15017f3bb3d1ecea538
Nick Chaffee <nchafee@gmail.com>
2019-07-02 17:35:50 -0400

    js queen-attack 2

--- javascript/queen-attack/queen-attack.js
+++ javascript/queen-attack/queen-attack.js
@@ -5,44 +5,34 @@ export class QueenAttack {
         QueenAttack.validate(this);
     }
 
-    canAttack() {
-        if (
-            this.white[0] == this.black[0] ||
-            this.white[1] == this.black[1] ||
-            (Math.abs(this.white[0] - this.white[1]) == Math.abs(this.black[0] - this.black[1])) ||
-            (this.white[0] - this.black[0]) == (this.black[1] - this.white[1])
-            )
-            { return true }
-        return false;
-    }
-
-    toString() {
-        let outputString = ''
-        for(let row = 0; row <= 7; row++){
-            for(let column = 0; column <= 7; column++){
-                let square = '';
-                switch(true) {
-                    case(this.white[0] == row && this.white[1] == column):
-                        square = 'W';
-                        break;
-                    case(this.black[0] == row && this.black[1] == column):
-                        square = 'B';
-                        break;
-                    default:
-                        square = '_';
-                        break;
-                }
-                if(column < 7){ square = square.concat(" ") }
-                outputString = outputString.concat(square)
-            }
-            outputString = outputString.concat('\n')
-        }
-        return outputString;
-    }
-
     static validate({ white: white, black: black} ) {
         if(white[0] == black[0] && white[1] == black[1]){ 
             throw new Error('Queens cannot share the same space')
         }
+    }
+
+    canAttack() { return this.sameColumn() || this.sameRow() || this.sameDiagonal() }
+    sameRow() { return this.white[0] == this.black[0] }
+    sameColumn() { return this.white[1] == this.black[1] }
+    sameDiagonal() { return this.xDiagonal() || this.otherDiagonal() }
+    xDiagonal() { return (Math.abs(this.white[0] - this.white[1]) == Math.abs(this.black[0] - this.black[1])) }
+    otherDiagonal() { return (this.white[0] - this.black[0]) == (this.black[1] - this.white[1]) }
+
+    toString() {
+        return (
+            new Array(8).
+            fill(new Array(8).fill('_')).
+            reduce((outputStr, row, rowIdx) =>
+                outputStr.concat(
+                    row.map((col, colIdx) => 
+                        rowIdx == this.white[0] && colIdx == this.white[1] ? 'W' : 
+                        rowIdx == this.black[0] && colIdx == this.black[1] ? 'B' : 
+                        col
+                    ).
+                    join(" ").
+                    concat("\n")
+                )
+            , "")
+            )
     }
 }
