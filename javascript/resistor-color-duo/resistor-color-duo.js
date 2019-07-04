fc99a72c7acb6f9623b01261207aca279b1ea4be
Nick Chaffee <nchafee@gmail.com>
2019-07-04 13:30:14 -0400

    js resistor-color-duo 2

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,9 +1,8 @@
 import { colorCode } from '../resistor-color/resistor-color'
 
-export const value = (color_words) => {
-  return parseInt(
-    color_words.
-      map(word => colorCode(word).toString())
-      .join("")
-  );
-};
+export const value = color_words =>
+  parseInt(
+    color_words.map(word =>
+      colorCode(word).toString()
+    ).join("")
+  )
