12313e7f6b981adcd8436ae4c1387656861664d6
Nick Chaffee <nchafee@gmail.com>
2019-07-04 11:56:40 -0400

    js gigasecond 2

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,6 +1,10 @@
-const GIGASECONDS = Math.pow(10, 9);
+const GIGASECOND = Math.pow(10, 9);
 const toSeconds = date => Date.parse(date) / 1000
 const toDate = seconds => new Date(1000 * seconds)
 
 export const gigasecond = date =>
-  toDate(toSeconds(date) + GIGASECONDS)
+  [date].
+    map(date => toSeconds(date)).
+    map(seconds => seconds + GIGASECOND).
+    map(seconds => toDate(seconds)).
+    shift()
