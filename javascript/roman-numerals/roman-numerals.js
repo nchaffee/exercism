1479ed7e09f4b1789e7691a3ea3f4038ee33767d
Nick Chaffee <nchafee@gmail.com>
2019-08-09 09:39:28 -0400

    js roman-numerals 2

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -1,47 +1,28 @@
-export const toRoman = (num) => {
-  let power = num.toString().length - 1;
-  let result = ""
+export const toRoman = (numero) => {
+  let risultado = '', resto = numero;
 
-  while(power >= 0) {
-    let tensDigit = Math.pow(10,power);
-    let base = parseInt(num / tensDigit) * tensDigit;
-    result = result.concat(romanLetterMap[base]);
-    num = num % tensDigit;
-    power--;
+  for (let [numero, romano] of dizionario.entries()) {
+    while(resto >= numero){
+      risultado += romano;
+      resto -= numero;
+    }
   }
-  return result;
-};
 
-const romanLetterMap = {
-  0:"",
-  1:"I",
-  2:"II",
-  3:"III",
-  4:"IV",
-  5:"V",
-  6:"VI",
-  7:"VII",
-  8:"VIII",
-  9:"IX",
-  10:"X",
-  20:"XX",
-  30:"XXX",
-  40:"XL",
-  50:"L",
-  60:"LX",
-  70:"LXX",
-  80:"LXXX",
-  90:"XC",
-  100:"C",
-  200:"CC",
-  300:"CCC",
-  400:"CD",
-  500:"D",
-  600:"DC",
-  700:"DCC",
-  800:"DCCC",
-  900:"CM",
-  1000:"M",
-  2000:"MM",
-  3000:"MMM"
+  return risultado;
 }
+
+const dizionario = new Map([
+  [1000,"M"],
+  [900,"CM"],
+  [500,"D"],
+  [400,"CD"],
+  [100,"C"],
+  [90,"XC"],
+  [50,"L"],
+  [40,"XL"],
+  [10,"X"],
+  [9,"IX"],
+  [5,"V"],
+  [4,"IV"],
+  [1,"I"]
+])
