e18786759cd0261dcac3d920fc22ac04db0b34f9
Nick Chaffee <nchafee@gmail.com>
2019-11-29 21:44:16 +0700

    ruby luhn 3

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,33 +1,40 @@
 class Luhn
+  def initialize(str)
+    @str = str.delete(" ")
+  end
+
+  def valid?
+    at_least_two_numbers? && valid_checksum?
+  end
+
+  private
+
+  def at_least_two_numbers?
+    @str.match?(/^\d{2,}$/)
+  end
+
+  def valid_checksum?
+    digits = @str.chars.map(&:to_i)
+    convert_to_doubles_and_sum(digits) % 10 == 0
+  end
+
+  def convert_to_doubles_and_sum(str)
+    str.reverse.map.with_index do |number, index|
+      second_digit?(index) ? limit_to_one_digit(number*2) : number
+    end.sum
+  end
+
+  def second_digit?(index)
+    (index + 1) % 2 == 0
+  end
+
+  def limit_to_one_digit(number)
+    number >= 10 ? number % 10 + 1 : number
+  end
+
   class << self
     def valid?(str)
-      str = str.gsub(" ", "")
-      longer_than_one?(str) && only_numbers?(str) && valid_checksum?(str)
+      Luhn.new(str).valid?
     end
-
-    def valid_checksum?(str)
-      digits = str.chars.map(&:to_i)
-      convert_to_doubles_and_sum(digits) % 10 == 0
-    end
-
-    def convert_to_doubles_and_sum(str)
-      str.reverse.map.with_index do |number, index|
-        second_digit?(index) ? limit_to_one_digit(number*2) : number
-      end.sum
-    end
-
-    def limit_to_one_digit(number)
-      more_than_10?(number) ? add_digits(number) : number
-    end
-
-    def more_than_10?(number) number >= 10; end
-
-    def second_digit?(index) (index + 1) % 2 == 0; end
-
-    def add_digits(number) number % 10 + 1; end
-
-    def longer_than_one?(str) str.length > 1; end
-
-    def only_numbers?(str) str.match?(/^\d*$/); end
   end
 end
