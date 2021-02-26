40bc57b88c0e8a0ae9474bbe51070fd6bc47a9e9
Nick Chaffee <nchafee@gmail.com>
2021-02-25 21:00:42 -0500

    ruby clock 2

--- ruby/clock/clock.rb
+++ ruby/clock/clock.rb
@@ -2,30 +2,29 @@ class Clock
     attr_reader :hour, :minute
 
     def initialize(hour:0,minute:0)
-        total_mins = hour * 60 + minute;
-        @hour = total_mins / 60 % 24;
-        @minute = total_mins % 60;
+        minute_to_hours, @minute = minute.divmod(60);
+        @hour = (hour + minute_to_hours) % 24;
     end
 
     def +(clock)
-        Clock.new(hour: @hour + clock.hour, minute: @minute + clock.minute)
+        Clock.new(
+            hour: @hour + clock.hour,
+            minute: @minute + clock.minute
+        )
     end
 
     def -(clock)
-        self.+ Clock.new(hour: clock.hour * -1, minute: clock.minute * -1)
+        Clock.new(
+            hour: @hour - clock.hour,
+            minute: @minute - clock.minute
+        )        
     end
 
     def ==(clock)
-        self.hour == clock.hour && self.minute == clock.minute
+        @hour == clock.hour && @minute == clock.minute
     end
 
     def to_s
-        "#{pad(@hour)}:#{pad(@minute)}"
-    end
-
-    private
-
-    def pad(num)
-        num.to_s.rjust(2, "0")
+        "%02d:%02d" % [@hour, @minute]
     end
 end
