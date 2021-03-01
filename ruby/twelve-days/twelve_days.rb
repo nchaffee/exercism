d93b82fe4018d2ea7f79390ae40d3306e3ba6ac3
Nick Chaffee <nchafee@gmail.com>
2021-03-01 11:44:37 -0500

    ruby twelve-days 3

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -31,12 +31,12 @@ class TwelveDays
 
     def self.song
         DAYS.each_with_index.inject("") do |output, (day, idx)|
-            gifts_for_this_day = GIFTS[(GIFTS.length - idx - 1), GIFTS.length]
-            last_gift = gifts_for_this_day.pop + "."
-            last_gift = "and " + last_gift if gifts_for_this_day.any?
-            gifts = gifts_for_this_day.push(last_gift).join(", ")
-            output << "\n\n" if idx > 0
-            output << "On the #{day} day of Christmas my true love gave to me: #{gifts}"
+            gifts = idx == 0 ? GIFTS[-1] : (
+                output << "\n\n"
+                gifts_for_this_day = GIFTS[(GIFTS.length - idx - 1), idx]
+                gifts = gifts_for_this_day.push("and " + GIFTS[-1]).join(", ")
+            )
+            output << "On the #{day} day of Christmas my true love gave to me: #{gifts}."
             output
         end + "\n"
     end
