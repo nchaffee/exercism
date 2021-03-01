b78ca2024edd980b1c68a809d0e380cb0a42226d
Nick Chaffee <nchafee@gmail.com>
2021-03-01 09:21:05 -0500

    ruby twelve-days 2

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -1,38 +1,43 @@
-=begin
-Write your code for the 'Twelve Days' exercise in this file. Make the tests in
-`twelve_days_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/twelve-days` directory.
-=end
-
 class TwelveDays
-    SONG = <<-SONG
-On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.
+    DAYS = [
+        "first",
+        "second",
+        "third",
+        "fourth",
+        "fifth",
+        "sixth",
+        "seventh",
+        "eighth",
+        "ninth",
+        "tenth",
+        "eleventh",
+        "twelfth",
+    ]
 
-On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.
+    GIFTS = [
+        "twelve Drummers Drumming",
+        "eleven Pipers Piping",
+        "ten Lords-a-Leaping",
+        "nine Ladies Dancing",
+        "eight Maids-a-Milking",
+        "seven Swans-a-Swimming",
+        "six Geese-a-Laying",
+        "five Gold Rings",
+        "four Calling Birds",
+        "three French Hens",
+        "two Turtle Doves",
+        "a Partridge in a Pear Tree"
+    ]
 
-On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-
-On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.
-    SONG
     def self.song
-        SONG
+        DAYS.each_with_index.inject("") do |output, (day, idx)|
+            gifts_for_this_day = GIFTS[(GIFTS.length - idx - 1), GIFTS.length]
+            last_gift = gifts_for_this_day.pop + "."
+            last_gift = "and " + last_gift if gifts_for_this_day.any?
+            gifts = gifts_for_this_day.push(last_gift).join(", ")
+            output << "\n\n" if idx > 0
+            output << "On the #{day} day of Christmas my true love gave to me: #{gifts}"
+            output
+        end + "\n"
     end
 end
