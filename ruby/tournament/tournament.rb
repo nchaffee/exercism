db1668b532a257ec3a2a5a5937dcfca25fb5fa87
Nick Chaffee <nchafee@gmail.com>
2021-03-02 10:37:05 -0500

    ruby tournament 2

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,45 +1,46 @@
 class Tournament
-    def self.tally(input)
-        matches = input.split("\n")
-        teams = []
-        if matches.any?
-            matches.each do |match|
-                team_name1,team_name2,result = match.split(";")
-                #set up teams
-                [team_name1,team_name2].each do |name|
-                    if teams.none? {|team| team.name == name}
-                        teams.push(Team.new(name))
-                    end
-                end
-                # record scores
-                team1 = teams.select{|team| team.name == team_name1}.first
-                team2 = teams.select{|team| team.name == team_name2}.first
-                case result
-                when "win"
-                    team1.win
-                    team2.loss
-                when "loss"
-                    team1.loss
-                    team2.win
-                when "draw"
-                    team1.draw
-                    team2.draw
-                end
+    attr_accessor :teams
+    def initialize(input)
+        @matches = input.split("\n")
+        @teams = []
+    end
+
+    def find_team(name)
+        if @teams.none? {|team| team.name == name}
+            @teams.push(Team.new(name))
+        end
+        @teams.find{|team| team.name == name}
+    end
+
+    def tally
+        @matches.each do |match|
+            name1,name2,result = match.split(";")
+            team1, team2 = find_team(name1), find_team(name2)
+            case result
+            when "win"
+                team1.win
+                team2.loss
+            when "loss"
+                team1.loss
+                team2.win
+            when "draw"
+                team1.draw
+                team2.draw
             end
         end
-        # output table
-        Table.new(teams).to_s
+    end
+
+    def self.tally(input)
+        tournament = Tournament.new(input)
+        tournament.tally
+        Table.new(tournament).to_s
     end
 
     class Team
         attr_reader :name, :played, :wins, :losses, :draws, :points
         def initialize(name)
             @name = name
-            @wins = 0
-            @losses = 0
-            @draws = 0
-            @points = 0
-            @played = 0
+            @wins = @losses = @draws = @points = @played = 0
         end
 
         def win
@@ -61,29 +62,25 @@ class Tournament
     end
 
     class Table
-        def initialize(teams)
-            @teams = teams
-            @output = ""
+        def initialize(tournament)
+            @teams = tournament.teams
         end
 
         def to_s
-            @output << header
+            output = table_line(["Team","MP","W","D","L","P"])
             if @teams.any?
-                # puts "@teams: #{@teams}"
                 sorted_teams = @teams.sort_by{|team| [(team.points * -1), team.name]}
-                # puts "sorted_teams: #{sorted_teams}"
                 sorted_teams.each do |team|
-                    @output << sprintf(
-                        "%-31s|%3d |%3d |%3d |%3d |%3d\n" %
-                        [team.name, team.played, team.wins, team.draws, team.losses, team.points]
-                    )
+                    columns = [team.name, team.played, team.wins, team.draws, team.losses, team.points].map(&:to_s)
+                    output << table_line(columns)
                 end
             end
-           @output
+            output
         end
 
-        def header
-            "Team                           | MP |  W |  D |  L |  P\n"
+        private
+        def table_line(columns)
+            sprintf("%-31s|%3s |%3s |%3s |%3s |%3s\n" % columns)
         end
     end
 end
