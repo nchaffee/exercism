75810d6fcf7498ffe9a450a8bdd8ed4aa0afbfdd
Nick Chaffee <nchafee@gmail.com>
2021-03-04 08:11:02 -0500

    ruby robot-name 2

--- ruby/robot-name/robot_name.rb
+++ ruby/robot-name/robot_name.rb
@@ -1,22 +1,16 @@
 class Robot
+    AVAILABLE_NAMES = ('AA000'..'ZZ999').to_a.freeze
+
     def self.forget
-        @@available_names = AVAILABLE_NAMES.dup
+        @@available_names = AVAILABLE_NAMES.dup.shuffle
     end
+    forget
 
-    LETTERS = ('A'..'Z').to_a
-    NUMBERS = (0..9).to_a.map(&:to_s)
-    ALL_NAMES = LETTERS.map{|letter1| LETTERS.map{|letter2| NUMBERS.map {|number1| NUMBERS.map{|number2| NUMBERS.map{|number3| letter1 + letter2 + number1 + number2 + number3} }}}}.flatten
-    AVAILABLE_NAMES = Hash[ALL_NAMES.each_with_object(nil).to_a]
-    @@available_names = AVAILABLE_NAMES.dup
-
-    attr_accessor :name
-    def initialize
-        reset
+    def name
+        @name ||= @@available_names.pop
     end
 
     def reset
-        name = @@available_names.first.first
-        @name = name
-        @@available_names.delete(name)
+        @name = nil
     end
 end
