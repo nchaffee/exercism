d41d6666c048c9e836b38ed32e4e95d3d4122ac7
Nick Chaffee <nchafee@gmail.com>
2021-03-05 09:53:21 -0500

    ruby bowling 1

--- ruby/bowling/bowling.rb
+++ ruby/bowling/bowling.rb
@@ -1,76 +1,69 @@
 class Game
-  class BowlingError < StandardError
-  end
-
-  def initialize
-    @frames = []
-  end
-
-  def roll(pins_down)
-    raise BowlingError unless pins_down >= 0 && pins_down <= 10
-    raise BowlingError if last_frame? && current_frame.length > 1 && current_frame.sum < 10
-
-    if @frames.any? && current_frame.length == 1 && current_frame.first != 10
-      raise BowlingError if current_frame.first + pins_down > 10
-      current_frame << pins_down
-    elsif last_frame?
-      if strike?(current_frame)
-        current_frame << pins_down if current_frame.length < 3
-        raise BowlingError if current_frame.length == 3 && current_frame[1] < 10 && (current_frame[1] + current_frame[2] > 10)
-      elsif current_frame.length == 2 && current_frame.sum == 10
-        current_frame << pins_down
-      end
-    else
-      @frames << [pins_down]
+    attr_accessor :score
+    def initialize
+        @frames = []
+        @first_throw = nil
     end
-  end
 
-  def score
-    raise BowlingError unless last_frame?
-    raise BowlingError if (strike?(current_frame) || current_frame[0] + current_frame[1] == 10) && current_frame.length < 3
-
-    @frames.each.with_index.inject(0) do |total, (frame, index)|
-      total += frame.sum
-
-      case index
-      when 9
-      when 8
-        total += next_frame(index).first if frame.sum == 10
-        total += next_frame(index)[0] if frame.length == 1
-      else
-        total += next_frame(index).first if frame.sum == 10
-        if frame.first == 10
-          total += next_frame(index).length == 1 ? next_next_frame(index).first : next_frame(index)[1]
+    def roll(pins)
+        raise BowlingError if pins < 0 || pins > 10
+        if @frames.size == 10 && @frames[-1].size == 2 && @frames[-1].sum < 10
+            raise BowlingError
         end
-      end
-
-      total
+        if @first_throw && @first_throw + pins > 10
+            raise BowlingError
+        elsif @first_throw && pins < 10
+            @frames << [@first_throw, pins]
+            @first_throw = nil
+        elsif last_frame?
+            last_frame = @frames[-1]
+            if last_frame.first == 10 && 
+                last_frame.size == 2 && 
+                last_frame[1] < 10 &&
+                last_frame[1] + pins > 10
+                raise BowlingError
+            end
+            last_frame << pins
+        elsif pins == 10
+            @frames << [pins]
+            @first_throw = nil
+        else
+            @first_throw = pins
+        end
     end
-  end
 
-  def strike?(frame)
-    frame.first == 10
-  end
+    def score
+        raise BowlingError if @frames.size < 10
+        @frames.each_with_index.map do |frame, idx|
+            score = frame.sum
+            if strike?(frame)
+                next_frame = @frames[idx+1]
+                if next_frame.size > 1
+                    score += @frames[idx+1][0] + @frames[idx+1][1]
+                else
+                    score += next_frame.first
+                    score += @frames[idx+2].first
+                end
+            elsif spare?(frame)
+                score += @frames[idx+1].first
+            end
+            score
+        end.sum
+    end
 
-  def next_next_frame(index)
-    @frames[index + 2]
-  end
+    private
+    def spare?(frame)
+        frame.size == 2 && frame.sum == 10
+    end
 
-  def next_frame(index)
-    @frames[index + 1]
-  end
+    def strike?(frame)
+        frame.size == 1 && frame.first == 10
+    end
 
-  private
+    def last_frame?
+        @frames.size == 10
+    end
 
-  def last_frame?
-    @frames.length == 10
-  end
-
-  def current_frame
-    @frames.last
-  end
-end
-
-module BookKeeping
-  VERSION = 3
+    class BowlingError < StandardError
+    end
 end
