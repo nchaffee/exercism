b2fc159ead86c445911a545c5f55df18afd413c4
Nick Chaffee <nchafee@gmail.com>
2021-03-08 11:44:38 -0500

    ruby binary-search-tree 1

---
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -0,0 +1,29 @@
+class Bst
+    attr_accessor :data, :left, :right
+    def initialize(num)
+        @data = num
+    end
+
+    def each
+        return all_data.to_enum unless block_given?
+        all_data.each {|node| yield(node)}
+    end
+
+    def all_data
+        data = [@data]
+        [@left, @right].each{|node| data << node.all_data if node}
+        data.flatten.sort
+    end
+
+    def insert(num)
+        num > @data ? insert_right(num) : insert_left(num)
+    end
+
+    def insert_left(num)
+        @left.nil? ? @left = Bst.new(num) : @left.insert(num)
+    end
+
+    def insert_right(num)
+        @right.nil? ? @right = Bst.new(num) : @right.insert(num)
+    end
+end
