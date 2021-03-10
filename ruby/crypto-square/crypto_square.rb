b3a307b4f14617b2a8140f768e6be87acbb33065
Nick Chaffee <nchafee@gmail.com>
2021-03-10 10:37:47 -0500

    ruby crypto-square 2

--- ruby/crypto-square/crypto_square.rb
+++ ruby/crypto-square/crypto_square.rb
@@ -1,28 +1,18 @@
 class Crypto
-    VAILD_CHARS = [*'0'..'9', *'a'..'z']
-
     def initialize(plaintext)
         @plaintext = plaintext
     end
 
     def ciphertext
-        chars = @plaintext.
-            chars.
-            map(&:downcase).
-            keep_if{|char| VAILD_CHARS.include?(char)}
-
+        chars = @plaintext.downcase.scan(/\w/)
         return "" if chars.none?
         num_rows = Math.sqrt(chars.size).ceil
+        while chars.size % num_rows > 0 do chars.push(" ") end
 
-        chars.
-            tap do |chars|
-                while chars.size % num_rows > 0 do chars.push(" ") end
-            end.
-            each_with_index.
+        chars.each_with_index.
             reduce(Array.new(num_rows, "")) do |cipher, (char, idx)|
                 cipher[idx % num_rows] += char
                 cipher
-            end.
-            join(" ")
+            end.join(" ")
     end
 end
