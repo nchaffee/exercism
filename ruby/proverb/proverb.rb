e3a022add5cb29d54e7858f3e32783fa52f4d0af
Nick Chaffee <nchafee@gmail.com>
2021-12-18 10:49:43 -0500

    ruby proverb 2

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -5,10 +5,8 @@ class Proverb
     end
 
     def to_s
-        @words
-            .each_cons(2)
-            .map{|word1, word2| "For want of a #{word1} the #{word2} was lost.\n"}
-            .tap{|lines| lines << "And all for the want of a %s%s." % [(@qualifier.to_s + " ").lstrip, @words.first]}
-            .join
+        @words.each_cons(2).with_object("") do |(word1, word2), memo|
+            memo << "For want of a #{word1} the #{word2} was lost.\n"
+        end + "And all for the want of a %s%s." % [(@qualifier.to_s + " ").lstrip, @words.first]
     end
 end
