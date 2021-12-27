40c14f09686b94b3e23be7ae74f7b99a147156ed
Nick Chaffee <nchafee@gmail.com>
2021-12-27 13:58:41 -0500

    ruby palindrome-products 2

--- ruby/palindrome-products/palindrome_products.rb
+++ ruby/palindrome-products/palindrome_products.rb
@@ -1,32 +1,29 @@
 require 'ostruct'
-
 class Palindromes
     def self.palindrome?(arr)        
         return true if arr.length <= 1
-        arr[0] == arr[-1] ? self.palindrome?(arr.slice(1,arr.length - 2)) : false
+        arr[0] == arr[-1] ? palindrome?(arr.slice(1,arr.length - 2)) : false
     end
     
-    attr_reader :smallest, :largest
-
     def initialize(max_factor:,min_factor:1)
-        @range = [*min_factor..max_factor]
-        @largest = OpenStruct.new(value: 0, factors: [])
-        @smallest = OpenStruct.new(value: nil, factors: [])
+        @min_factor, @max_factor = min_factor, max_factor
     end
 
     def generate
-        @range.product(@range).each do |factors| 
-            value = factors.inject(&:*)
-            new_palindrome(value, factors.sort) if Palindromes.palindrome?(value.digits)
-        end
+        @palindromes = 
+            (@min_factor..@max_factor).
+                to_a.
+                repeated_combination(2).
+                group_by{|a,b| a * b}.
+                select{|value, _factors| Palindromes.palindrome?(value.digits)}.
+                map{|value, factors| OpenStruct.new(value: value, factors: factors)}
     end
 
-    private
-    def new_palindrome(value, factors)
-        @largest = OpenStruct.new(value: value, factors: [factors]) if value > @largest.value
-        @smallest = OpenStruct.new(value: value, factors: [factors]) if @smallest.value.nil? || value < @smallest.value
+    def largest
+        @palindromes.max{|a,b| a.value <=> b.value }
+    end
 
-        @largest.factors << factors if value == @largest.value && !@largest.factors.include?(factors)
-        @smallest.factors << factors if value == @smallest.value && !@smallest.factors.include?(factors)
+    def smallest
+        @palindromes.min{|a,b| a.value <=> b.value }
     end
 end
