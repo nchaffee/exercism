3a39a496d45e977e9e88b6d62dc40e5076593af4
Nick Chaffee <nchafee@gmail.com>
2021-12-31 18:54:38 -0500

    ruby perfect-numbers 5

--- ruby/perfect-numbers/perfect_numbers.rb
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -2,7 +2,9 @@ class PerfectNumber
     def self.classify(int)
         raise RuntimeError unless int.positive?
         below_int = 1..(int - 1)
-        case below_int.select {|num| (int % num).zero? }.sum
+        aliquot_sum = below_int.select {|num| (int % num).zero? }.sum
+
+        case aliquot_sum
         when below_int then "deficient"
         when int then "perfect"
         else "abundant" 
