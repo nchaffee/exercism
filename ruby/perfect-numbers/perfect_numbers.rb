8f3c82768b37e27b0aa462c4e56e7ef5c338ac72
Nick Chaffee <nchafee@gmail.com>
2021-12-31 17:52:04 -0500

    ruby perfect-numbers 2

--- ruby/perfect-numbers/perfect_numbers.rb
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -1,18 +1,7 @@
-require 'prime'
-
 class PerfectNumber
     def self.classify(int)
-        raise RuntimeError unless int > 0
-        case
-        when self.factors(int).sum == int then "perfect"
-        when self.factors(int).sum >= int then "abundant"
-        when self.factors(int).sum <= int then "deficient"
-        end
-    end
-
-    def self.factors(int)
-        (1..(int-1)).each_with_object([]) do |num, factors|
-            factors << num if int % num == 0
-        end
+        raise RuntimeError unless int.positive?
+        sum = (1..(int-1)).sum { |num| int % num == 0 ? num : 0 }
+        sum == int ? "perfect" : sum >= int ? "abundant" : "deficient"
     end
 end
--- ruby/perfect-numbers/perfect_numbers_test.rb
+++ ruby/perfect-numbers/perfect_numbers_test.rb
@@ -8,13 +8,13 @@ class PerfectNumberTest < Minitest::Test
     end
   end
 
-  def test_factors
-    assert_equal [1], PerfectNumber.factors(13)
-    assert_equal [1,2,3], PerfectNumber.factors(6)
-    assert_equal [1, 2, 4, 7, 14], PerfectNumber.factors(28)
-    assert_equal [1, 2, 3, 4, 6], PerfectNumber.factors(12)
-    assert_equal [1, 2, 3, 4, 6, 8, 12], PerfectNumber.factors(24)
-  end
+  # def test_factors
+  #   assert_equal [1], PerfectNumber.factors(13)
+  #   assert_equal [1,2,3], PerfectNumber.factors(6)
+  #   assert_equal [1, 2, 4, 7, 14], PerfectNumber.factors(28)
+  #   assert_equal [1, 2, 3, 4, 6], PerfectNumber.factors(12)
+  #   assert_equal [1, 2, 3, 4, 6, 8, 12], PerfectNumber.factors(24)
+  # end
 
   def test_classify_deficient
     assert_equal 'deficient', PerfectNumber.classify(13)
