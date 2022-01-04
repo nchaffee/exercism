f48254f085889828097d8c28dd4493b373a06b98
Nick Chaffee <nchafee@gmail.com>
2022-01-03 19:51:39 -0500

    ruby binary-search 2

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -1,9 +1,22 @@
 class BinarySearch
-    def initialize(arr)
-        @arr = arr
+    def initialize(list)
+        @list = list.sort
+        @first = 0
+        @last = list.length - 1
     end
 
-    def search_for(something)
-        @arr.index(something)
+    def search_for(target)
+        return nil if @list.empty? || @first + 1 == @last
+        return @first if @list[@first] == target
+        return @last if @list[@last] == target
+        middle = (@last - @first) / 2 + @first
+        return middle if @list[middle] == target
+        if @list[middle] > target
+            @last = middle
+            search_for(target)
+        else
+            @first = middle
+            search_for(target)
+        end
     end
 end
