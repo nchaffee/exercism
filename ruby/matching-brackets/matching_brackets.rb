a59c3fbf06e86913f64a65c11294e38d4252e2ab
Nick Chaffee <nchafee@gmail.com>
2022-01-06 16:10:53 -0500

    ruby matching-brackets 2

--- ruby/matching-brackets/matching_brackets.rb
+++ ruby/matching-brackets/matching_brackets.rb
@@ -3,19 +3,15 @@ class Brackets
         "]" => "[",
         "}" => "{",
         ")" => "("
-    }
+    }.freeze
 
     def self.paired?(str)
-        str
-            .gsub(/[^\[\]\{\}\(\)]|\s/, '')
-            .chars
-            .each_with_object([]) do |char, pairs|
-                if PAIR.values.include?(char)
-                    pairs.push(char)
-                else
-                    last = pairs.pop 
-                    return false if last != PAIR[char]
-                end
-            end.length.zero?
+        str.chars.each_with_object([]) do |char, pairs|
+            if PAIR.values.include?(char)
+                pairs.push(char)
+            elsif PAIR.keys.include?(char)
+                return false if pairs.pop != PAIR[char]
+            end
+        end.empty?
     end
 end
