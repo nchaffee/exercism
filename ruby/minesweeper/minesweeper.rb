bb74cafa3d3337cd37932172e123d70f213f06a8
Nick Chaffee <nchafee@gmail.com>
2022-01-09 21:54:59 -0500

    ruby minesweeper 2

--- ruby/minesweeper/minesweeper.rb
+++ ruby/minesweeper/minesweeper.rb
@@ -1,26 +1,57 @@
 class Board
     def self.transform(rows)
+        new(rows).to_s
+    end
+
+    MINE = '*'
+    INVALID = %w{X}
+    VALID = %w{+ - * |}
+
+    def initialize(rows)
+        validate(rows)
+        @rows = build(rows)
+        sweep
+    end
+
+    def validate(rows)
         raise ArgumentError if rows.any? do |row|
-            row[0] == '*'|| row.chars.include?('X') || row.length != rows.first.length
+            row[0] == MINE ||
+            row.chars.any?{|char| INVALID.include?(char)} ||
+            row.length != rows.first.length
         end
-        num_rows = rows.map(&:chars).map do |row|
-            row.map do |square|
-                %w{+ - * |}.include?(square) ? square : 0
+    end
+
+    def build(rows)
+        rows.map(&:chars).map do |row|
+            row.map do |sqr|
+                VALID.include?(sqr) ? sqr : 0
             end
         end
-        num_rows.each.with_index do |row, y|
+    end
+
+    def mine?(y,x)
+        @rows[y][x] == MINE
+    end
+
+    def increment_adjactent(y,x)
+        (y-1).upto(y+1) do |yy|
+            (x-1).upto(x+1) do |xx|
+                @rows[yy][xx] += 1 if @rows[yy][xx].class == Integer
+            end
+        end
+    end
+
+    def sweep
+        @rows.each.with_index do |row, y|
             row.each.with_index do |sqr, x|
-                if num_rows[y][x] == "*"
-                    (y-1).upto(y+1) do |yy|
-                        (x-1).upto(x+1) do |xx|
-                            num_rows[yy][xx] += 1 if num_rows[yy][xx].class == Integer
-                        end
-                    end
-                end
+                increment_adjactent(y,x) if mine?(y,x)
             end
         end
-        num_rows.map do |row|
-            row.map{|square| square == 0 ? " " : square}
+    end
+
+    def to_s
+        @rows.map do |row|
+            row.map{|sqr| sqr == 0 ? " " : sqr}
         end.map(&:join)
     end
 end
