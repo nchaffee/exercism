e79eacdb941ac7f44bc525d657720f8565fed545
Nick Chaffee <nchafee@gmail.com>
2022-01-11 15:20:21 -0500

    ruby atbash-cipher 6

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -2,19 +2,15 @@ module Atbash
     ALPHABET = [*'a'..'z']
     CODE = ALPHABET.zip(ALPHABET.reverse).to_h
 
-    def encode(text)
-        code(text).each_slice(5).map(&:join).join(' ')
-    end
-
     def decode(text)
-        code(text).join
+        text.downcase.chars
+        .select{|c| c =~ /[[:alnum:]]/}
+        .map{|c| CODE[c] ? CODE[c] : c}
+        .join
     end
 
-    module_function
-    def code(text)
-        text.downcase.chars
-            .select{|c| c =~ /[[:alnum:]]/}
-            .map{|c| CODE[c] ? CODE[c] : c}
+    def encode(text)
+        decode(text).chars.each_slice(5).map(&:join).join(' ')
     end
 end
 Atbash.extend(Atbash)
