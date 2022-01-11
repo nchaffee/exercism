aaf0b57c65a27314c620adc8389ff8478a0e648d
Nick Chaffee <nchafee@gmail.com>
2022-01-11 10:35:31 -0500

    ruby secret-handshake 2

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -8,16 +8,22 @@ class SecretHandshake
     ]
 
     def initialize(num)
-        @bits = num.is_a?(Integer) ? num.to_s(2).chars.map(&:to_i).reverse : []
+        @num = num
     end
 
     def commands
-        return [] if @bits.empty?
-        @bits.zip(COMMANDS).each_with_object([]) do |(bit, (match,command)), c|
-            if bit == 1 
-                c.reverse! if match == 10000
-                c.push(command) if match != 10000
+        return [] unless @num.is_a?(Integer)
+        @num
+            .to_s(2)
+            .chars
+            .map(&:to_i)
+            .reverse
+            .zip(COMMANDS)
+            .each_with_object([]) do |(bit, (_match,command)), c|
+                unless bit.zero?
+                    c.reverse! and next if command.split.first == "Reverse"
+                    c.push(command)
+                end
             end
-        end.compact
     end
 end
