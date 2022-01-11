fd13e9a8408eb2d6051346b2db61f2e2196c4e3f
Nick Chaffee <nchafee@gmail.com>
2022-01-11 11:13:26 -0500

    ruby secret-handshake 4

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -7,14 +7,8 @@ class SecretHandshake
 
     def commands
         return [] unless @num.is_a?(Integer)
-        @num
-            .to_s(2)
-            .chars
-            .map(&:to_i)
-            .reverse
-            .zip(COMMANDS)
-            .each_with_object([]) do |(bit, command), c|
-                unless bit.zero?
+        COMMANDS.each_with_object([]) do |command, c|
+                if @num & (2 ** COMMANDS.index(command)) > 0
                     c.reverse! and next if /Reverse/ =~ command
                     c.push(command)
                 end
