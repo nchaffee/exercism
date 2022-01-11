ab2d0054d57a16777a2f527ab8801d6798e83bc8
Nick Chaffee <nchafee@gmail.com>
2022-01-11 14:49:56 -0500

    ruby atbash-cipher 4

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,19 +1,19 @@
 class Atbash
     ALPHABET = [*'a'..'z']
-    ENCODE = ALPHABET.zip(ALPHABET.reverse).to_h
+    CODE = ALPHABET.zip(ALPHABET.reverse).to_h
 
-    def self.encode(text, separator = ' ')
-        text
-            .chars
-            .map(&:downcase)
-            .reject{|c| c =~ /[^[:alnum:]]/}
-            .map{|c| ENCODE[c] ? ENCODE[c] : c}
-            .each_slice(5)
-            .map(&:join)
-            .join(separator)
+    def self.encode(text)
+        code(text).each_slice(5).map(&:join).join(' ')
     end
 
     def self.decode(text)
-        encode(text, '')
+        code(text).join
+    end
+
+    private
+    def self.code(text)
+        text.downcase.chars
+            .select{|c| c =~ /[[:alnum:]]/}
+            .map{|c| CODE[c] ? CODE[c] : c}
     end
 end
