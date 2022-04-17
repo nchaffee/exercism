a3c36a1ee4a9693cb1787a7bee7f07ab6106f4ed
Nick Chaffee <nchafee@gmail.com>
2022-04-17 19:46:12 -0400

    js meetup passing all tests

--- javascript/meetup/meetup.js
+++ javascript/meetup/meetup.js
@@ -1,34 +1,70 @@
-const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 // Sunday is 0, Saturday is 6
-const firstDay = {
-  '2013': {
-    '5': 3
-  }
+const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const daysInMonth = {
+    '1': 31,
+    '2': 28,
+    '3': 31,
+    '4': 30,
+    '5': 31,
+    '6': 30,
+    '7': 31,
+    '8': 31,
+    '9': 30,
+    '10': 31,
+    '11': 30,
+    '12': 31,
 };
+const firstDay = {
+  '2012': {
+    '2': 3,
+  },
+  '2013': {
+    '1': 2,
+    '2': 5,
+    '3': 5,
+    '4': 1,
+    '5': 3,
+    '6': 6,
+    '7': 1,
+    '8': 4,
+    '9': 0,
+    '10': 2,
+    '11': 5,
+    '12': 0
+  },
+  '2014': {
+    '12': 1
+  },
+  '2015': {
+    '2': 0,
+  },
+};
+
 export const meetup = (year, month, typeOfDay, dayOfWeek)  => {
   const yearStr = `${year}`
   const monthStr = `${month}`
   // group number days by day of the week
   const firstDayOfTheMonth = firstDay[yearStr][monthStr]
-  console.log(firstDayOfTheMonth)
   let datesWeekdays = {};
   daysOfTheWeek.forEach(weekday => {
     datesWeekdays[weekday] = [];
   })
-  for (var day = 1; day <= 31; day++) {
+  let totalDays = daysInMonth[monthStr]
+  if(year == 2012 && month == 2) {totalDays = 29}
+  for (var day = 1; day <= totalDays; day++) {
     let today = (day - 1 + firstDayOfTheMonth) % 7
     let weekday = daysOfTheWeek[today]
     datesWeekdays[weekday].push(day)
   }
   
-  const meetupWeekday = datesWeekdays[dayOfWeek]
+  const meetupWeekdayDates = datesWeekdays[dayOfWeek]
   let meetupDay;
-  if (typeOfDay == 'teenth') {
-    meetupDay = meetupWeekday.find(date => date > 12);
-    console.log(meetupDay)
+  if(typeOfDay == 'teenth') {
+    meetupDay = meetupWeekdayDates.find(date => date > 12);
+  } else {
+    const ordinals = {'first': 0, 'second': 1, 'third': 2, 'fourth': 3, 'last': (meetupWeekdayDates.length - 1)}
+    meetupDay = meetupWeekdayDates[ordinals[typeOfDay]];
   };
   
-  return new Date(`${year}-${month}-${meetupDay}`);
+  return new Date(year, month - 1, meetupDay);
 };
-
-expect(meetup(2013, 5, 'teenth', 'Monday')).toEqual(new Date(2013, 4, 13));
