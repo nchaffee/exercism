7698a8fd7f0edebfc47b6772220e7eee97729b6d
Nick Chaffee <nchafee@gmail.com>
2022-04-23 10:56:26 -0400

    js run-length-encoding 2

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -1,37 +1,11 @@
 export const encode = (str) => {
-  if (str.length < 2) {return str}
-  let encoded = '';
-  let buffer = 1;
-  for (let i = 0; i < str.length; i++) {
-    let currentLetter = str[i];
-    let nextLetter = str[i+1];
-    if(currentLetter === nextLetter) {
-      buffer++;
-    } else if(buffer === 1) {
-      encoded += currentLetter;
-    } else {
-      encoded += buffer + currentLetter;
-      buffer = 1;
-    }
-  }
-  return encoded;
+  return str.replace(/(\w|\s)\1+/g, (match, parenSubmatch) => {
+    return `${match.length}${parenSubmatch}`
+  });
 };
 
 export const decode = (str) => {
-  if (str.length < 2) {return str}
-  let decoded = '';
-  let multiplier = '';
-  for (let i = 0; i < str.length; i++) {
-    let currentLetter = str[i];
-    let nextLetter = str[i+1];
-    if(currentLetter.match(/[0-9]/)) {
-      multiplier += currentLetter;
-    } else if(multiplier === '') {
-      decoded += currentLetter;
-    } else {
-      decoded += currentLetter.repeat(parseInt(multiplier));
-      multiplier = '';
-    }
-  }
-  return decoded;
+  return str.replace(/(\d+)(\w|\s)/g, (_match, paren1, paren2) => {
+    return paren2.repeat(parseInt(paren1));
+  });
 };
