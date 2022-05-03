06e3f7115839f20369ab12107a15ff51fa54138e
Nick Chaffee <nchafee@gmail.com>
2022-05-03 10:42:25 -0400

    js allergies 2

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -1,10 +1,8 @@
 const allergens = ['eggs','peanuts','shellfish','strawberries','tomatoes','chocolate','pollen','cats']
 export class Allergies {
-  constructor(score){ this.score = score }
-
-  list = () => allergens.filter(allergen => 
-    ((2 ** allergens.indexOf(allergen)) & this.score) > 0
-  )
-
-  allergicTo = (allergen) => this.list().includes(allergen)
+  constructor(score){
+    this.list = () => allergens.filter(allergen => 
+      (2 ** allergens.indexOf(allergen) & score) > 0)
+    this.allergicTo = (allergen) => this.list().includes(allergen)
+  }
 }
